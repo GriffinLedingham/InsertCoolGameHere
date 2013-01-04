c2ffbd1a345cb397202a4f4d87b42a028153bff3
Paul <toadums@hotmail.com>
2013-01-03 20:14:28 -0800

    Initial commit.
    I set up the project using SharpDX. Now all YOU GUYS have to do is add
    the code. You just add files, like you would add them to a normal C#
    file, and yeah.
    
    It is VERY IMPORTANT TO NOTE THAT when you guys checkout the project,
    you need to REMOVE my sharpDX dlls from the REFERENCES folder. then RE
    ADD them from YOUR LOCAL folder.
    
    <3

---
+++ Felicitas/Felicitas/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Felicitas.Resources;
+
+namespace Felicitas {
+    public partial class MainPage : PhoneApplicationPage {
+
+        Platformer MainGame;
+
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+
+        }
+
+        private void BackGroundGrid_Loaded(object sender, RoutedEventArgs e) {
+
+            MainGame = new Platformer();
+            MainGame.Run(BackgroundGrid);
+
+        }
+
+    }
+}
---
+++ Felicitas/Felicitas/Platformer.cs
@@ -0,0 +1,54 @@
+﻿using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Felicitas {
+    class Platformer : Game {
+
+        public GraphicsDeviceManager graphicsDeviceManager;
+        public SpriteBatch spriteBatch;
+
+
+
+        public Platformer()
+        {
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+            graphicsDeviceManager.SynchronizeWithVerticalRetrace = false;
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void BeginRun()
+        {
+            base.BeginRun();
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Felicitas";
+            base.Initialize();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
